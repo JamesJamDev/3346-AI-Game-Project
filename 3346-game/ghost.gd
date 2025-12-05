13309dd0e13c3bea7338f11730193042c7bba887
pal bhatia <bhatia.pal@gmail.com>
2025-12-05 10:10:55 -0500

    pink tries to move 4 tiles ahead of you

--- 3346-game/ghost.gd
+++ 3346-game/ghost.gd
@@ -2,13 +2,20 @@ extends CharacterBody2D
 
 @onready var agent: NavigationAgent2D = $NavigationAgent2D
 @onready var pacman := $"../Pacman"
+@export var type : GhostType
 
-func _ready() -> void:
-	agent.target_position = pacman.global_position
+enum GhostType {Blinky, Pinky, Inky, Clyde}
+
 
 func _physics_process(delta: float) -> void:
-	# Continuously update target
-	agent.target_position = pacman.global_position
+	
+	if type == GhostType.Blinky:
+		# Continuously update target
+		agent.target_position = pacman.global_position
+	
+	if type == GhostType.Pinky:
+		agent.target_position = pacman.prediction_spot.global_position
+	
 
 	# Get next path point
 	var next_point = agent.get_next_path_position()
--- 3346-game/pacman_manager.gd
+++ 3346-game/pacman_manager.gd
@@ -5,8 +5,11 @@ class_name Player
 var movement_direction = Vector2.ZERO
 
 
+
 @export var speed = 700
 @export var start_position: Node2D
+@onready var prediction_spot: Node2D = $RayCast2D/predictionSpot
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
 
 
 @onready var collision_shape_2d = $CollisionShape2D
@@ -21,9 +24,17 @@ func reset_player():
 func _physics_process(delta):
 	if movement_direction != Vector2.ZERO:
 		rotation = movement_direction.angle()
+		ray_cast_2d.target_position = movement_direction * 70
+
+	ray_cast_2d.rotation = (movement_direction * Vector2(0, -1)).angle() # Invert Y axis vector
 
 	velocity = movement_direction * speed * delta
 	move_and_slide()
+	
+	if ray_cast_2d.is_colliding():
+		prediction_spot.global_position = ray_cast_2d.get_collision_point()
+	else:
+		prediction_spot.global_position = ray_cast_2d.to_global(ray_cast_2d.target_position)
 
 
 func _unhandled_input(event: InputEvent) -> void:
